d91f64d8022a6e1e688eda3283e15d15a94f5fb8
stevemacharia <stevemachariah@outlook.com>
2023-01-23 15:40:53 +0300

    Zamil start

--- main/views.py
+++ main/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+def AboutUs(request):
+    return render(request, 'main/about_us.html')
